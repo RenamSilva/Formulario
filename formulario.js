207affb0be0f4dbc148dfb78a8c09dfd2e397ef4
RenamS <renams.alves@gmail.com>
2022-05-12 21:36:35 -0300

    Trabalho final

--- formulario.js
+++ formulario.js
@@ -3,7 +3,7 @@ const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const passwordConfirmation = document.getElementById("password-confirmation");
-const submit = document.getElementById("submit");
+
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -20,36 +20,46 @@ function checkInputs() {
 
     if (usernameValue === "") {
         setErrorFor(username, "Insira seu nome.");
+        document.getElementById("botao").disabled = false;
     } else if (usernameValue.length < 3) {
         setErrorFor(username, "Por favor, insira um nome com pelo menos 3 caracteres.")
+
     } else if (usernameValue.length > 25) {
         setErrorFor(username, "Por favor, insira um nome com menos de 25 carateres.")
+
     } else {
         setSuccsessFor(username);
     }
 
     if (emailValue === "") {
         setErrorFor(email, "Insira seu endereço de email.");
+
     } else if (!checkEmail(emailValue)) {
         setErrorFor(email, "Por favor, insira um email válido.");
+
     } else {
         setSuccsessFor(email);
     }
 
     if (passwordValue === "") {
         setErrorFor(password, "Insira uma senha.");
+
     } else if (passwordValue.length < 8) {
         setErrorFor(password, "A senha precisa ter no mínimo 8 caracteres.");
+
     } else {
         setSuccsessFor(password);
     }
 
     if (passwordConfirmationValue === "") {
         setErrorFor(passwordConfirmation, "Por favor, confirme sua senha.");
+
     } else if (passwordConfirmationValue !== passwordValue) {
         setErrorFor(passwordConfirmation, "A senha de confirmação deve ser igual a senha.");
+
     } else {
         setSuccsessFor(passwordConfirmation);
+        document.getElementById("botao").disabled = false;
     }
 
     const formControls = form.querySelectorAll(".form-control");
@@ -59,7 +69,7 @@ function checkInputs() {
     });
 
     if (formIsValid) {
-        alert("Cadastro efetuado com sucesso!");
+
     }
 }
 
@@ -82,4 +92,35 @@ function checkEmail(email) {
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
         email
     );
+}
+
+let emailLista = []
+let usuario = []
+function verificacaoBotao() {
+    document.getElementById("botao").classList.add("botao-cinza")
+    document.getElementById("botao").disabled = true;
+
+    for (let i = 0; i <= usuario.length; i++) {
+        if (username.value == usuario[i]) {
+            alert("Nome de usuário já existe")
+            document.getElementById("botao").disabled = false;
+            return
+        }
+    }
+    for (let i = 0; i <= emailLista.length; i++) {
+        if (email.value == emailLista[i]) {
+            alert("Endereço de email já existe")
+            document.getElementById("botao").disabled = false;
+            return
+        }
+    }
+    emailLista.push(email.value)
+    usuario.push(username.value)
+
+    setTimeout(
+        function ativar_botao() {
+            document.getElementById("botao").disabled = false;
+            document.getElementById("botao").classList.remove("botao-cinza")
+        }, 1000
+    )
 }
